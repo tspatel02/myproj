4df67fabe5c8bb3c045dcb90a8247c33de984de7
Patel <212066255@logon.ds.ge.com>
2017-06-21 23:09:21 -0500

    first project

---
+++ testmvn/src/main/java/com/mycomp/mydept/testmvn/Student.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycomp.mydept.testmvn;
+
+/**
+ *
+ * @author 212066255
+ */
+public class Student {
+    private String first;
+    private String last;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "first=" + first + ", last=" + last + '}';
+    }
+
+    public Student() {
+    
+    }
+    
+}
---
+++ testmvn/src/test/java/test/com/StudentTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import com.mycomp.mydept.testmvn.Student;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 212066255
+ */
+public class StudentTest {
+    
+    public StudentTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+     
+    @Test
+     public void name() {
+     Student s = new Student();
+     s.setFirst("tom");
+     s.setLast("patel");
+     
+        assertEquals("tompatel", s.getFirst()+s.getLast());
+     
+     }
+}
